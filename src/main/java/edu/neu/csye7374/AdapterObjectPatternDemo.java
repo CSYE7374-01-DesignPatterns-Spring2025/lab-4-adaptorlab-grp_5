35d04701072a26bc50d7ca88f8d277f30c2b4c3a
Harsh Shah <shah.harsh9@northeastern.edu>
2025-02-04 20:36:05 -0500

    Final working code

--- src/main/java/edu/neu/csye7374/AdapterObjectPatternDemo.java
+++ src/main/java/edu/neu/csye7374/AdapterObjectPatternDemo.java
@@ -1,20 +1,26 @@
 package edu.neu.csye7374;
+
 import java.util.List;
+
 public class AdapterObjectPatternDemo {
-    public static void Demo() {
-        CalculatableAPI calculator = new Calculator();
+	public static void Demo() {
+		CalculatableAPI calculator = new Calculator();
         AccumulatableAPI interfaceRegister = new CalculatorAdapter(calculator);
+
         List<Double> prices = List.of(10.0, 20.5, 5.25);
         double total1 = interfaceRegister.accumulation(prices);
         System.out.println("Total Accumulated Price: $" + total1);
+
         double remainingBalance1 = interfaceRegister.payment(50.0);
         System.out.println("Remaining Balance after Payment: $" + remainingBalance1);
-
+        
         AccumulatableAPI register2 = new CalculatorClassAdapter();
         List<Double> prices2 = List.of(10.0, 22.5, 5.25);
         double total2 = register2.accumulation(prices2);
         System.out.println("Total Accumulated Price: $" + total2);
+
         double remainingBalance2 = register2.payment(50.0);
         System.out.println("Remaining Balance after Payment: $" + remainingBalance2);
-    }
+	}
+
 }
